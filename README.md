e1c27630b1057371fda6a8f8b4260e5a624bc77c
zhangx <zhangxin@fafafa.io>
2019-05-21 14:59:23 +0800

    react
    [dev] add 描述文件

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is learn react demo!
+
+接口:   https://music.aityp.com
+API:    https://binaryify.github.io/NeteaseCloudMusicApi/#/
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import './header.css';
 
-const HeaderTitleItem = ['发现音乐', '我的音乐', '朋友', '商城', '音乐人', '下载客户端']
+const HeaderTitleItem = ['发现音乐', '我的音乐', '朋友', '商城', '音乐人', '下载客户端'];
+
+class HeaderNavLi extends React.Component {
+    render() {
+        if (this.props.item === '下载客户端') {
+            return (
+                <li key={this.props.item} className="lst">
+                    <span>
+                        <a hidefocus="true" className="z-slt" href="/#">
+                            <em>{this.props.item}</em>
+                            <sub className="cor">&nbsp;</sub>
+                        </a>
+                    </span>
+                    <sup className="hot">&nbsp;</sup>
+                </li>
+            )
+        } else {
+            return (
+                <li key={this.props.item}>
+                    <span>
+                        <a hidefocus="true" href="/#">
+                            <em>{this.props.item}</em>
+                            <sub className="cor">&nbsp;</sub>
+                        </a>
+                    </span>
+                </li>
+            )
+        }
+
+    }
+}
 
 class HeaderNav extends React.Component {
     render() {
         const listItems = HeaderTitleItem.map((number) =>
-            <li key={number}>
-                <span>
-                    <a hidefocus="true" className="z-slt" href="/#">
-                        <em>{number}</em>
-                        <sub className="cor">&nbsp</sub>
-                    </a>
-                </span>
-            </li>
+            <HeaderNavLi item={number} key={number} />
         );
         return (
             <ul className="m-nav j-tflag" >{listItems}</ul>
@@ -25,13 +48,16 @@ class Header extends React.Component {
 
     render() {
         return (
-            <div className="music-top m-top">
-                <div className="content f-cb">
-                    <h1 className="music-logo logo">
-                        <a href="/#" hidefocus="true">网易云音乐</a>
-                    </h1>
-                    <HeaderNav />
+            <div>
+                <div className="music-top m-top">
+                    <div className="content f-cb">
+                        <h1 className="music-logo logo">
+                            <a href="/#" hidefocus="true">网易云音乐</a>
+                        </h1>
+                        <HeaderNav />
+                    </div>
                 </div>
+                <div className="m-subnav m-subnav-up f-pr j-tflag"></div>
             </div>
         )
     }
